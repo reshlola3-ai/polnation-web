67098e8a211a84d5bd800dd7d10d53d2cc3593e7
lucypandahey <lucypandahey@gmail.com>
2026-02-16 11:12:09 +0800

    fix: remove dynamic import causing build error in server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import dynamic from 'next/dynamic'
 import { cookies } from 'next/headers'
 import { createServerClient } from '@/lib/supabase-server'
 import { Navbar } from '@/components/layout/Navbar'
 import { FeaturesSection } from '@/components/home/FeaturesSection'
+import { ChainStats } from '@/components/home/ChainStats'
 import { ArrowRight, Sparkles } from 'lucide-react'
-
-// Lazy load below-the-fold components (ChainStats is further down)
-const ChainStats = dynamic(
-  () => import('@/components/home/ChainStats').then(mod => mod.ChainStats),
-  { ssr: false, loading: () => <div className="h-64" /> }
-)
 import { getTranslations } from 'next-intl/server'
 import { defaultLocale, locales, type Locale } from '@/i18n/config'
 
