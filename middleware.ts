5fcb0458ca495062725ac12687ffc4894da8edbd
lucypandahey <lucypandahey@gmail.com>
2026-04-21 16:10:07 +0800

    feat: migrate homepage to static polygon clone

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,10 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.rewrite(new URL('/polygon-clone/index.html', request.url))
+  }
+
   let supabaseResponse = NextResponse.next({
     request,
   })
